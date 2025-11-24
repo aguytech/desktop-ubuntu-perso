35ea23323c36dd221358e2ac1e07635605b0b85a
aguy tech <aguytech@free.fr>
2025-11-24 15:18:45 +0100

    perso*: few corrections

--- perso.sh
+++ perso.sh
@@ -4,13 +4,13 @@
 
 S_TRACE=debug
 _PATH_BASE=$( readlink -f ${0%/*} )
-_PATH_CONF=${HOME}/.config/desktop-install
+_PATH_CONF=/usr/local/conf
 _PATH_LOG=/var/log/desktop-install
 _CMD="sudo apt"
 _CMD_INS="sudo apt install -y"
 
 # inc
-file=${_PATH_BASE}/bs/inc
+file=${_path_base}/bs/inc
 ! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
 ! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
 
@@ -23,5 +23,7 @@ parts_perso="global conf root mozilla"
 for _PART in ${parts_perso}; do
 	_source_sub "${_PART}" ${subpart}
 done
+
+_echoa "All parts has done"
 
 _exit
