8840ec858a3b01d953cb2862ad30dc9f1e950f0f
aguy tech <aguytech@free.fr>
2025-11-30 00:00:21 +0100

    perso.sh: add check for internet connection

--- perso.sh
+++ perso.sh
@@ -15,6 +15,10 @@ file=${_path_base}/bs/inc
 ! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
 ! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
 
+########################  WWW
+
+sudo ping -c1 google.com >/dev/null || _exite "Installation needs internet connection"
+
 ########################  VARIABLES
 
 export DEBIAN_FRONTEND=noninteractive
